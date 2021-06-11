16a7642962d2db9d2fd0d8ab750dab4e5199ba2c
Euro <no>
2021-06-10 21:15:12 -0700

    fix: circular import

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # TML
 
-tml is a markup language specifically designed to be displayed in terminals, it is interpreted at runtime with `main.py` however you can write the output to a file, although it will be missing features like centered text.
+tml is a markup language specifically designed to be displayed in terminals, it is interpreted at runtime with `main.py` however you can write the output to a file, although it will be missing features.
 
+Features missing when not interprated at runtime
+* hr (horizontal rules), the length is calculated at runtime, so if written, the line may be too long/short
 
 a sample tml file
 ```tml
---
+++ elements.py
@@ -0,0 +1,191 @@
+from typing import List
+
+COLORS = {
+    "black": "30",
+    "default": "0",
+    "red": "31",
+    "green": "32",
+    "yellow": "33",
+    "blue": "34",
+    "magenta": "35",
+    "cyan": "36",
+    "white": "37"
+}
+
+BACKGROUND_COLORS = {
+    "black": "40",
+    "red": "41",
+    "green": "42",
+    "yellow": "43",
+    "blue": "44",
+    "magenta": "45",
+    "cyan": "46",
+    "white": "47",
+    "default": "0"
+}
+
+TEXT_STYLES = {
+    "bold": "1",
+    "dim": "2",
+    "italic": "3",
+    "underline": "4",
+    "blinking": "5",
+    "inverse": "7",
+    "invisible": "8",
+    "strikethrough": "9"
+}
+
+class Document:
+    def __init__(self, children):
+        self.Parent = None
+        self.children = children
+        self.innerHTML = ""
+        self.styles = tuple()
+
+    def addChild(self, element):
+        self.children.append(element)
+
+    def __repr__(self):
+        return "<!DOCUMENT>"
+
+document = Document([])
+
+class Element:
+    def __init__(self, tag, attrs, parent=Document, children=None):
+        self.tag = tag
+        self.attrs = attrs
+        self.parent = parent
+        self.children: List[Element] = children or []
+        self.styles = []
+        self.gap = 1 #the multiplier for the gap, 1 is normal 2, is twice the default
+        self._parseAtters()
+        self.selfClosing = False
+
+    def _parseAtters(self):
+        for attr, value in self.attrs:
+            if attr == "color":
+                color = COLORS.get(value)
+                if not color: continue
+                self.styles.append(color)
+            elif attr in ("background", "bg", "background-color", "bg-color"):
+                color = BACKGROUND_COLORS.get(value)
+                if not color: continue
+                self.styles.append(color)
+            elif attr == "gap":
+                self.gap = int(value)
+
+    def addChild(self, element):
+        self.children.append(element) 
+
+    def render(self): return ""
+
+    def __repr__(self):
+        return f'<{self.tag}{self.attrs}>{self.children}</{self.tag}>'
+
+
+class BoldElement(Element):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.styles.append(TEXT_STYLES["bold"])
+
+    def render(self):
+        newLines = "\n" * self.gap if self.gap != 1 else ""
+        return "\033[1m" + parseChildren(self) + "\033[0m" + newLines
+
+class DimElement(Element):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.styles.append(TEXT_STYLES["dim"])
+
+    def render(self):
+        newLines = "\n" * self.gap if self.gap != 1 else ""
+        return "\033[2m" + parseChildren(self) + "\033[0m" + newLines
+
+class ItalicElement(Element):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.styles.append(TEXT_STYLES["italic"])
+
+    def render(self):
+        newLines = "\n" * self.gap if self.gap != 1 else ""
+        return "\033[3m" + parseChildren(self) + "\033[0m" + newLines
+
+class UnderlineElement(Element):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.styles.append(TEXT_STYLES["underline"])
+
+    def render(self):
+        newLines = "\n" * self.gap if self.gap != 1 else ""
+        return "\033[4m" + parseChildren(self) + "\033[0m" + newLines
+
+class BlinkElement(Element):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.styles.append(TEXT_STYLES["blinking"])
+
+    def render(self):
+        newLines = "\n" * self.gap if self.gap != 1 else ""
+        return "\033[5m" + parseChildren(self) + "\033[0m" + newLines
+
+class ReverseElement(Element):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.styles.append(TEXT_STYLES["inverse"])
+
+    def render(self):
+        newLines = "\n" * self.gap if self.gap != 1 else ""
+        return "\033[7m" + parseChildren(self) + "\033[0m" + newLines
+
+class InvisibleElement(Element):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.styles.append(TEXT_STYLES["invisible"])
+
+    def render(self):
+        newLines = "\n" * self.gap if self.gap != 1 else ""
+        return "\033[8m" + parseChildren(self) + "\033[0m" + newLines
+
+class StrikethroughElement(Element):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.styles.append(TEXT_STYLES["strikethrough"])
+
+    def render(self):
+        newLines = "\n" * self.gap if self.gap != 1 else ""
+        return "\033[9m" + parseChildren(self) + "\033[0m" + newLines
+
+class ParagraphElement(Element):
+    def render(self):
+        return parseChildren(self) + ("\n\n" * self.gap)
+
+class BreakElement(Element):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.selfClosing = True
+    def render(self):
+        return "\n" * self.gap
+
+
+class TextElement:
+    def __init__(self, text, parent=None):
+        self.text = text.strip()
+        self.parent = parent
+        self.styles = self.parent.styles
+
+    def render(self):
+        return self.text + "\033[0m"
+
+    def __repr__(self):
+        return f'{self.text}'
+
+
+def parseChildren(element: Element):
+    text = ""
+    for child in element.children:
+        for style in child.parent.styles:
+            text += f'\033[{style}m'
+        for style in child.styles:
+            text += f'\033[{style}m'
+        text += child.render()
+    return text
